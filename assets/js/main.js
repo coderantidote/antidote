df5ba8491a5a4fdc053496e889da1a059591cb89
Antidote <antidotedefacer@gmail.com>
2019-10-24 03:12:47 +0300

    Hot Fix
    Index pagination system + blog pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,14 @@
- 
+ $(document).ready(function() {
+     $('#thread_table').DataTable({
+         "order": [
+             [2, "desc"]
+         ],
+         columnDefs: [
+             { width: 500, targets: 0 },
+             { width: 100, targets: 1 },
+             { width: 100, targets: 2 },
+             { width: 100, targets: 3 }
+         ],
+         fixedColumns: true
+     });
+ });

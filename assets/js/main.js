04bec41899c28b73fd488e8ec15cc5237abfc166
Antidote <antidotedefacer@gmail.com>
2019-10-28 14:13:40 +0300

    Fix Posts side
    x

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,28 @@
- $(document).ready(function() {
-     $('#thread_table').DataTable({
-         "order": [
-             [2, "desc"]
-         ],
-         columnDefs: [
-             { width: 500, targets: 0 },
-             { width: 100, targets: 1 },
-             { width: 100, targets: 2 },
-             { width: 100, targets: 3 }
-         ],
-         fixedColumns: true
-     });
- });
+function createTable(jsonData) {
+    $(document).ready(function() {
+        $.get("json/posts.json", function(data) {
+            $('#thread_table').dataTable({
+                data: data,
+                columns: [
+                    { data: 'title' },
+                    { data: 'categories' },
+                    { data: 'date' },
+                    { data: 'loc' }
+                ],
+                "order": [
+                    [2, "desc"]
+                ],
+                columnDefs: [
+                    { width: 500, targets: 0 },
+                    { width: 100, targets: 1 },
+                    { width: 100, targets: 2 },
+                    { width: 100, targets: 3 }
+                ],
+                fixedColumns: true
+            });
+        });
+    });
+}
+
+
+createTable();
